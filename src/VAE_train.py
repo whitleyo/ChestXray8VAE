cce1f93877d163dd21afe0f25b000e9a1cc66c1e
whitleyo <owen.whitley@mail.utoronto.ca>
2021-01-15 15:18:48 -0500

    add necessary global variables for VAE_train.py

--- src/VAE_train.py
+++ src/VAE_train.py
@@ -1,9 +1,13 @@
 import VAE
 import os
 import torch
+import numpy as np
 
 from skimage import io
 from warnings import warn
+
+# set random seed
+np.random.seed(42)
 
 # setup directories
 # top_dir = 'D:/projects/chest_XRay_8'
@@ -14,6 +18,11 @@ output_dir = os.path.join(top_dir, 'results')
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
+# setup global variables for mean and std dev of intensity based on sandbox notebook
+# TODO: setup to calculate these stats based on random sample of images
+mean_intensity = np.float(123.5)
+std_intensity = np.float(58.2)
+
 # setup dataset
 table_data = pd.read_csv(os.path.join(data_dir, 'Data_Entry_2017_v2020.csv'))
 table_data = table_data.iloc[0:500, :]
@@ -23,10 +32,9 @@ VAE_DS = VAE.XRayDataset(table_data=table_data,
 
 # setup trainer and train
 VAE_Trainer = VAE.Trainer(XRayDS=VAE_DS)
-
 n_epochs = 5
 VAE_Trainer.train(num_epochs=n_epochs, batch_size=100)
 # save model state
 FinalModel = VAE_Trainer.Model
-torch.save(FinalModel.state_dict(), path=os.path.join(output_dir, 'VAE_state_dict_trained.pt'))
+torch.save(FinalModel.state_dict(), os.path.join(output_dir, 'VAE_state_dict_trained.pt'))
 
